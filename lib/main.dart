3414fa032572fe7aa5dc16722964a1bbfd2bab9d
young <beste3374@gmail.com>
2024-03-31 18:47:26 +0900

    pubpec에 assets 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:health/screen/HomeScreen.dart';
 
 void main() {
- runApp(MyApp());
- print('hello world');
-}
-
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(
-        home: Scaffold(
-          body: Center(child: Text('캡스톤2024')),
-        ),
-
-    );
-  }
+ runApp(homescreen());
 }
---
+++ lib/screen/HomeScreen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+
+class homescreen extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          child: Text('HI'),
+        ),
+      ),
+
+    );
+  }
+}
