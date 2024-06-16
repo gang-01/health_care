b2bfb1a72dba66555ea1112a9fa086ac591b7d4e
young <beste3374@gmail.com>
2024-06-16 14:32:27 +0900

    최종

--- lib/screen/Disease.dart
+++ lib/screen/Disease.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:excel/excel.dart';
-
+import 'dart:io';
 
 import 'package:flutter/services.dart' show ByteData, rootBundle;
+import 'package:get/get.dart';
 
 class DiseaseInputScreen extends StatefulWidget {
   @override
@@ -12,7 +13,14 @@ class DiseaseInputScreen extends StatefulWidget {
 class _DiseaseInputScreenState extends State<DiseaseInputScreen> {
   final _controller = TextEditingController();
   String _result = '';
+  String _result1='';
+  String _result2='';
+  String _result3='';
+
   Map<String, List<String>> _medicineMap = {};
+  Map<String, List<String>> _medicineMap1 = {};
+  Map<String, List<String>> _medicineMap2 = {};
+  Map<String, List<String>> _medicineMap3 = {};
 
   @override
   void initState() {
@@ -21,23 +29,29 @@ class _DiseaseInputScreenState extends State<DiseaseInputScreen> {
   }
 
   Future<void> _loadExcelData() async {
-    ByteData data= await rootBundle.load('assets/medicine.xlsx');
-    final bytes= data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
-    final excel= Excel.decodeBytes(bytes);
-
+    final bytes = await rootBundle.load('assets/medicine.xlsx');
+    final excel = Excel.decodeBytes(bytes.buffer.asUint8List(bytes.offsetInBytes, bytes.lengthInBytes));
 
     for (var table in excel.tables.keys) {
       var sheet = excel.tables[table];
       for (var row in sheet!.rows) {
         String? disease = row[0]?.value.toString();
-        String? medicine = row[1]?.value.toString();
+        String? medicine = row[1]?.value.toString(); //약
+        String? act = row[2]?.value.toString(); //증상
+        String? age = row[3]?.value.toString(); //연령
+        String? hospital=row[4]?.value.toString(); //병원
 
         if (_medicineMap.containsKey(disease)) {
           _medicineMap[disease]!.add(medicine!);
+          _medicineMap1[disease]!.add(act!);
+          _medicineMap2[disease]!.add(age!);
+          _medicineMap3[disease]!.add(hospital!);
         } else {
           _medicineMap[disease!] = [medicine!];
+          _medicineMap1[disease!]=[act!];
+          _medicineMap2[disease!]=[age!];
+          _medicineMap3[disease!]=[hospital!];
         }
-        print("added $medicine for disease $disease");
       }
     }
   }
@@ -45,15 +59,32 @@ class _DiseaseInputScreenState extends State<DiseaseInputScreen> {
   void _searchMedicine() {
     final disease = _controller.text;
     final medicines = _medicineMap[disease] ?? ['추천 약을 찾을 수 없습니다'];
+    final acts = _medicineMap1[disease] ??['없습니다.'];
+    final ages = _medicineMap2[disease] ??[''];
+    final hos = _medicineMap3[disease] ??[''];
 
     setState(() {
       _result = medicines.join(', ');
+     _result1 = acts.join(',');
+      _result2 = ages.join(',');
+      _result3 = hos.join(',');
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          '약 추천',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        backgroundColor: Colors.lightGreen, // Light green background color for AppBar
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
@@ -71,7 +102,110 @@ class _DiseaseInputScreenState extends State<DiseaseInputScreen> {
               child: Text('약 추천'),
             ),
             SizedBox(height: 20),
-            Text('추천 약: $_result'),
+            //Text('추천 약: $_result'),
+            Expanded(
+              child: GridView.count(
+              crossAxisCount: 2,
+              children: [
+                Center(
+                  child: Container(
+                      width: 150,
+                      height: 100,
+                      color: Colors.white10,
+                      margin: EdgeInsets.all(5.0),
+                      child: Text(
+                        '일어날 수 있는 증상입니다 =>',
+                        style: TextStyle(color: Colors.black,fontSize: 11,fontWeight: FontWeight.bold),
+                      ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.greenAccent,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      ':$_result',
+                      style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 150,
+                    height: 100,
+                    color: Colors.white,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '걸릴 수 있는 연령입니다 =>',
+                      style: TextStyle(color: Colors.black,fontSize: 11,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.greenAccent,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '$_result1',
+                      style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 150,
+                    height: 100,
+                    color: Colors.white,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '추천 약입니다 꼭드세요! =>',
+                      style: TextStyle(color: Colors.black,fontSize: 11,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.greenAccent,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '$_result2',
+                      style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 150,
+                    height: 100,
+                    color: Colors.white,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '주의주의!! 예방하세요 =>',
+                      style: TextStyle(color: Colors.black,fontSize: 11,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    width: 200,
+                    height: 100,
+                    color: Colors.greenAccent,
+                    margin: EdgeInsets.all(5.0),
+                    child: Text(
+                      '$_result3',
+                      style: TextStyle(color: Colors.black,fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+              ],
+              ),
+            ),
           ],
         ),
       ),
